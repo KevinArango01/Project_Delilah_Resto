701eb3626a56d1974757ae78e4486d4f09417267
Kevin Arango <kevin.arango@viliv.com.co>
2021-08-09 09:53:01 -0500

    Finish project

--- app.js
+++ app.js
@@ -1,25 +1,68 @@
 require('dotenv').config()
 const express = require('express')
 
-
 const port = process.env.PORT || 3000
 
 const app = express()
 
 const AuthMiddleware = require('./lib/interfaces/middlewares/AuthMiddleware')
+const PermissionMiddleware = require('./lib/interfaces/middlewares/PermissionMiddleware')
 
+//USERS
 const {
-    sinIn,
+    signIn,
+    signUp,
+    getUserData,
 } = require('./lib/interfaces/controllers/UsersController')
+
+//FOODS
 const {
     getFoods,
+    createFood,
+    updateFood,
+    deleteFoodById,
 } = require('./lib/interfaces/controllers/FoodsController')
+
+//ORDERSDETAILS
+const {
+    getAllDetailsByIdOrder,
+    addDetail,
+    updateDetail,
+    deleteDetail,
+} = require('./lib/interfaces/controllers/DetailsController')
+
+//ORDERS
+const {
+    createOrder,
+    getAllOrders,
+    updateStatusOrder,
+} = require('./lib/interfaces/controllers/OrdersController')
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/foods', AuthMiddleware, getFoods)
-app.post('/login', sinIn)
+//USERS
+app.post('/signin', signIn)
+app.post('/signup', signUp)
+app.get('/userdata/:id', AuthMiddleware, getUserData)
+
+//FOODS
+app.get('/foods', getFoods)
+app.post('/food', AuthMiddleware, PermissionMiddleware, createFood)
+app.put('/food/:id', AuthMiddleware, PermissionMiddleware, updateFood)
+app.delete('/food/:id', AuthMiddleware, PermissionMiddleware, deleteFoodById)
+
+//DETAILS
+app.get('/shoppingcart/:id',AuthMiddleware, getAllDetailsByIdOrder)
+app.post('/itemshoppingcart',AuthMiddleware, addDetail)
+app.put('/itemshoppingcart/:id',AuthMiddleware, updateDetail)
+app.delete('/itemshoppingcart/:id',AuthMiddleware, deleteDetail)
+
+//ORDERS
+app.post('/generateorder',AuthMiddleware, createOrder)
+app.get('/orders',AuthMiddleware, getAllOrders)
+app.put('/order/updatestatus/:id',AuthMiddleware, PermissionMiddleware, updateStatusOrder)
 
 app.listen(port, () => console.log(`Listening on: http://localhost:${port}`))
 
